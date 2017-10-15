7b72c04c96cf01aca3b42f40cef1e17bc71d2610
Louis-Saglio <louis.saglio@sfr.fr>
2017-10-15 02:20:32 +0200

    Refactor du flux logique

--- incremental_save/main.py
+++ incremental_save/main.py
@@ -3,33 +3,40 @@ import sys
 import os
 
 
+# CONFIG_FILE = os.path.normpath(os.path.expanduser("~/.pysync"))
+CONFIG_FILE = os.path.normpath(os.path.expanduser("~/Cours/Scripting/python_script/incremental_save/config.json"))
+
+
+def raise_error(message, return_code=1):
+    print(message)
+    sys.exit(return_code)
+
+
 def arguments_exists(args):
     return bool(args)
 
 
 def arguments_are_valid(args):
-    if len(args) == 2:
-        return True
-    elif args[1][-1] == os.sep:
-        print("Nom d'archive invalide")
-        sys.exit(1)
-    else:
-        print("Arguments invalides")
-        sys.exit(1)
+    return len(args) == 2
 
 
 def standardize_argument_input(args):
-    list_dest_path = args[1].split(os.sep)
+    dest_path, archive_name = os.path.split(args[1])
     return {
-        "src_path": os.path.normpath(args[0]) + os.sep,
-        "dest_path": os.path.join(*list_dest_path[:-1]) + os.sep,
-        "archive_name": list_dest_path[-1]
+        "src_path": os.path.abspath(args[0]) + os.sep,
+        "dest_path": os.path.abspath(dest_path + os.sep),
+        "archive_name": archive_name
     }
 
 
 def load_json(json_path):
-    with open(json_path, "r") as f:
-        return json.load(f)
+    try:
+        with open(json_path, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        raise_error(f"Aucun fichier {json_path} trouvé")
+    except json.JSONDecodeError:
+        raise_error("Json invalide")
 
 
 def json_is_valid(json_dict):
@@ -41,22 +48,26 @@ def standardize_json_input(json_dict):
 
 
 def input_data_is_valid(input_data):
-    return os.path.isdir(input_data["src_path"])
+    return os.path.isdir(input_data["src_path"]) and input_data["archive_name"]
 
 
 if __name__ == '__main__':
+
     arguments = sys.argv[1:]
-    json_conf = load_json('./config.json')
-    if arguments_exists(arguments) and arguments_are_valid(arguments):
+
+    if arguments:
+        if not arguments_are_valid(arguments):
+            raise_error("Arguments invalides")
         data = standardize_argument_input(arguments)
-    elif json_is_valid(json_conf):
-        data = standardize_json_input(json_conf)
     else:
-        print("Aucune donnée valide trouvée")
-        sys.exit(1)
+        json_conf = load_json(CONFIG_FILE)
+        if not json_is_valid(json_conf):
+            raise_error("Json invalide")
+        data = standardize_json_input(json_conf)
+
     if not input_data_is_valid(data):
-        print(data)
-        print("Données invalides")
-        sys.exit(1)
-    input(data)
-    os.makedirs(data["dest_path"], 600, True)
+        raise_error(f"Données invalides\t:\t{data}")
+    os.makedirs(data["dest_path"], 0o600, True)
+    if not os.path.isdir(data["src_path"]):
+        raise_error(f"La source {data['src_path']} n'existe pas ou n'est pas un dossier")
+    print("Synchronisation effectuée avec succès !", data)
